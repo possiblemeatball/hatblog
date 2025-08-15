7f6c1db56f7367990b70843c7d5de0e292bf26ea
possiblemeatball <meatball@manthrowshat.net>
2025-08-15 13:55:39 -0400

    Add sticky to sidebar nav so it does not scroll with content

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -8,12 +8,15 @@ const { site, frontmatter } = useData()
 
 <template>
   <div class="grid grid-flow-row md:grid-flow-col">
-    <Sidebar class="md:row-span-3 not-md:pt-12 not-md:pb-4 md:pr-4" />
-    <Content class="md:col-span-12 p-4 pt-0 min-h-svh
+    <Sidebar class="md:sticky md:top-4 md:self-start md:row-span-3
+                    not-md:pt-4 not-md:pb-4 md:pr-4" />
+    <div class="md:col-span-12 md:overflow-y-auto">
+      <Content class="p-4 pt-0 min-h-svh
                     bg-neutral-200 dark:bg-neutral-800
                     prose text-justify
                     prose-p:font-sans prose-h3:font-display
                     prose-neutral dark:prose-invert" />
-    <Footer class="md:col-span-12 " />
+      <Footer />
+    </div>
   </div>
 </template>
