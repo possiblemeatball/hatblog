d74fe64c12f300523a6b440509495e4980774b07
possiblemeatball <meatball@manthrowshat.net>
2025-08-24 20:47:43 -0400

    format dates in a nicer way so it looks better

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -4,6 +4,9 @@ import Footer from "./components/Footer.vue";
 import Sidebar from "./components/Sidebar.vue";
 
 const {frontmatter} = useData()
+const formatter = new Intl.DateTimeFormat('en-US', {
+  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'short'
+});
 </script>
 
 <template>
@@ -14,8 +17,12 @@ const {frontmatter} = useData()
       <div class="p-4 min-h-svh
                   bg-neutral-200 dark:bg-neutral-800">
         <div class="font-serif prose max-w-2xl text-justify prose-neutral dark:prose-invert">
-          <h1 class="mb-1 text-left">{{ frontmatter.title }}</h1>
-          <p class="mt-1 text-left text-sm text-gray-700 dark:text-gray-400 italic">{{ frontmatter.description }}</p>
+          <h1 class="mt-0 mb-0 text-left" v-if="frontmatter.title">{{ frontmatter.title }}</h1>
+          <p class="mt-0 mb-0 text-left text-gray-700 dark:text-gray-400 italic" v-if="frontmatter.description">{{ frontmatter.description }}</p>
+          <p class="mt-0 text-left text-gray-700 dark:text-gray-400" v-if="frontmatter.date || frontmatter.author">
+            <span v-if="frontmatter.date">published on {{ formatter.format(new Date(frontmatter.date as string)) }}</span>
+            <span v-if="frontmatter.author"> by <a :href="frontmatter.author.link">{{ frontmatter.author.name }}</a></span>
+          </p>
 
           <Content />
         </div>
--- Newsfeed.vue
+++ Newsfeed.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
 import { data as posts } from './posts.data'
-
+const formatter = new Intl.DateTimeFormat('en-US', {
+  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'short'
+});
 </script>
 
 <template>
   <div v-for="post in posts" class="select-none">
-    <div v-if="post.frontmatter.date">
-      <h2 class="mt-0 pt-4 mb-0">
-        <a :href="'/blog'+post.url">{{ post.frontmatter.title }}</a>
-      </h2>
-      <p class="mt-0 mb-2 text-sm text-gray-700 dark:text-gray-400 italic">
-        {{ post.frontmatter.description }}
-      </p>
-      <div class="prose-p:mt-2 prose-p:mb-2" v-html="post.excerpt" />
-      <p class="mt-0 mb-4 text-sm text-gray-700 dark:text-gray-400">
-        published on {{ new Date(post.frontmatter.date).toLocaleString() }}
-        by <a :href="post.frontmatter.author.link">{{ post.frontmatter.author.name }}</a>
-      </p>
-    </div>
+    <h2 class="mt-0 pt-4 mb-0">
+      <a :href="'/blog'+post.url">{{ post.frontmatter.title }}</a>
+    </h2>
+    <p class="mt-0 mb-2 text-sm text-gray-700 dark:text-gray-400 italic">
+      {{ post.frontmatter.description }}
+    </p>
+    <div class="prose-p:mt-2 prose-p:mb-2" v-html="post.excerpt" />
+    <p class="mt-0 mb-4 text-sm text-gray-700 dark:text-gray-400">
+      published on {{ formatter.format(new Date(post.frontmatter.date)) }}
+      by <a :href="post.frontmatter.author.link">{{ post.frontmatter.author.name }}</a>
+    </p>
   </div>
 </template>
 
